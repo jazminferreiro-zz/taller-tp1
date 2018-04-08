e7bc453143fe6e8c2ac8bfa29ecff6d1a4740fc3
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-04-08 13:03:23 -0300

    tp1 sockets

---
+++ tp.c
@@ -0,0 +1,39 @@
+#define _POSIX_C_SOURCE 200112L
+#include "client.h"
+#include "server.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#define SUCCESS 0
+#define ERROR 1
+
+
+int main(int argc, char *argv[]){
+   if (argc < 4){
+    printf("Parametros incorrectos. \n");
+    return ERROR;
+  }
+  int is_client = strcmp(argv[1],"client"); 
+  if (is_client == 0){
+    if (argc < 6){
+      printf("Parametros incorrectos. \n");
+      return ERROR;
+    }
+    return client(argv[2],atoi(argv[3]),argv[4],argv[5]);
+  }
+  return server(atoi(argv[2]),argv[3]);
+}
+
+
+
+
+
