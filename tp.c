e853f0545915d9442b4b17d06bccd370d04370c8
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2018-04-15 15:26:20 -0300

    agregando constantes a tp.c

--- tp.c
+++ tp.c
@@ -16,22 +16,34 @@
 #define SUCCESS 0
 #define ERROR 1
 
+#define MIN_ARGUMENTS_EXPECTED 4
+#define CLIENT_ARGUMENTS_EXPECTED 6
+#define TRUE 0
+
+#define CLIENT_IP 2
+#define CLIENT_PORT 3
+#define CLIENT_KEY 4
+#define CLIENT_FILE_NAME 5
+
+#define SERVER_PORT 2
+#define SERVER_KEY 3
 
 int main(int argc, char *argv[]){
-   if (argc < 4){
+   if (argc < MIN_ARGUMENTS_EXPECTED){
     printf("Parametros incorrectos. \n");
     return ERROR;
   }
   int is_client = strcmp(argv[1],"client"); 
-  if (is_client == 0){
-    if (argc < 6){
+  if (is_client == TRUE){
+    if (argc < CLIENT_ARGUMENTS_EXPECTED){
       printf("Parametros incorrectos. \n");
       return ERROR;
     }
-    return client(argv[2],atoi(argv[3]),argv[4],argv[5]);
+    return client(argv[CLIENT_IP],atoi(argv[CLIENT_PORT]),argv[CLIENT_KEY],argv[CLIENT_FILE_NAME]);
   }
-  return server(atoi(argv[2]),argv[3]);
+  return server(atoi(argv[SERVER_PORT]),argv[SERVER_KEY]);
 }
+
 
 
 
